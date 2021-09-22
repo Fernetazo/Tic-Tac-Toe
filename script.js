0adc664ac6c9912ec174db5df9ec7f566d38f023
Fernetazo <angelofdarkness88@gmail.com>
2021-09-22 19:56:56 -0300

    check for winner function (horizontal and vertical)

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const gameBoard = ["", "", "", "", "", "", "", "", ""];
+const gameBoard = ['', '', '', '', '', '', '', '', ''];
 let globalTurn = null;
 
 const selectBlock = (e) => {
 
     let block = e.target;
     
-    if (gameBoard[block.classList[1]] === "") {
+    if (gameBoard[block.classList[1]] === '') {
         if (game.getTurn() === player1.getName()) {
-            block.textContent = "❌";
+            block.textContent = '❌';
             game.setTurn(player2.getName());
             display.textContent = `Is the turn of: ${game.getTurn()}`;
-            gameBoard[block.classList[1]] = "❌";
+            gameBoard[block.classList[1]] = '❌';
     
         } else {
-            block.textContent = "🔵";
+            block.textContent = '🔵';
             game.setTurn(player1.getName());
             display.textContent = `Is the turn of: ${game.getTurn()}`;
-            gameBoard[block.classList[1]] = "🔵";
+            gameBoard[block.classList[1]] = '🔵';
         }
     }
-    console.log(gameBoard);
+    game.checkWinner();
 }
 
 const player = (name) => {
@@ -41,7 +41,7 @@ const displayController = (() => {
 
 const renderBoard = () => {
 
-    let blocks = document.querySelectorAll(".block");
+    let blocks = document.querySelectorAll('.block');
     
     blocks.forEach((element, index) => {
 
@@ -56,12 +56,53 @@ const game = (() => {
 
     const setTurn = (turn) => globalTurn = turn;
 
-    return {getTurn, setTurn};
+    const checkWinner = () => {
+
+        let checker = 0;
+        
+        for (let index = 0; index < 8;) {
+            for (let subIndex = 0; subIndex < 3; subIndex++) {
+                let block = gameBoard[index + subIndex];
+                if (block === '❌') {
+                    checker++;
+                    if (checker === 3) {
+                        display.textContent = `THE WINNER IS: ${player1.getName()}!!!`;
+                        return (player1.getName());
+                    }
+                } else {
+                    checker = 0;
+                    break;
+                }
+            }
+            index = index + 3;
+        }
+        
+        checker = 0;
+
+        for (let index = 0; index < 3; index++) {
+            for (let subIndex = 0; subIndex < 8;) {
+                let block = gameBoard[index + subIndex];
+                if (block === '❌') {
+                    checker++;
+                    subIndex = subIndex + 3;
+                    if (checker === 3) {
+                        display.textContent = `THE WINNER IS: ${player1.getName()}!!!`;
+                        return (player1.getName());
+                    }
+                } else {
+                    checker = 0;
+                    break;
+                }
+            }
+        }
+    };
+
+    return {getTurn, setTurn, checkWinner};
 })();
 
 game.setTurn(player1.getName());
 
-let display = document.querySelector(".display");
+let display = document.querySelector('.display');
 display.textContent = `Is the turn of: ${game.getTurn()}`;
 
 renderBoard();

f7984f73a34b90da63168a3490d3eca07b58f984
Fernetazo <angelofdarkness88@gmail.com>
2021-09-22 09:20:27 -0300

    Put player symbol in the grid

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const gameBoard = ["X", "O", "X", "X", "X", "O", "O", "O", "X"];
+const gameBoard = ["", "", "", "", "", "", "", "", ""];
+let globalTurn = null;
+
+const selectBlock = (e) => {
+
+    let block = e.target;
+    
+    if (gameBoard[block.classList[1]] === "") {
+        if (game.getTurn() === player1.getName()) {
+            block.textContent = "❌";
+            game.setTurn(player2.getName());
+            display.textContent = `Is the turn of: ${game.getTurn()}`;
+            gameBoard[block.classList[1]] = "❌";
+    
+        } else {
+            block.textContent = "🔵";
+            game.setTurn(player1.getName());
+            display.textContent = `Is the turn of: ${game.getTurn()}`;
+            gameBoard[block.classList[1]] = "🔵";
+        }
+    }
+    console.log(gameBoard);
+}
 
 const player = (name) => {
-    
+
     const getName = () => name;
     
-    const play = () => {
-        // PLAYER PLAYS
-    }
-
-    return {getName, play};
+    return {getName, selectBlock};
 };
+
+const player1 = player('Fer');
+const player2 = player('Vero');
 
 const displayController = (() => {
 
@@ -23,8 +44,24 @@ const renderBoard = () => {
     let blocks = document.querySelectorAll(".block");
     
     blocks.forEach((element, index) => {
+
         element.textContent = gameBoard[index];
+        element.addEventListener('click', selectBlock);
     });
 };
+
+const game = (() => {
+
+    const getTurn = () => globalTurn;
+
+    const setTurn = (turn) => globalTurn = turn;
+
+    return {getTurn, setTurn};
+})();
+
+game.setTurn(player1.getName());
+
+let display = document.querySelector(".display");
+display.textContent = `Is the turn of: ${game.getTurn()}`;
 
 renderBoard();
